13129bb8816679a1f0a09113d3b3a20212f694f3
Act <thecanbeat@gmail.com>
2016-08-23 10:01:09 -0600

    added location and info boxes and background

--- app.js
+++ app.js
@@ -1,5 +1,25 @@
+var timeouts = {},
+  d = {
+    p1i: document.getElementById("p1i"),
+    p2i: document.getElementById("p2i"),
+    infobox: document.getElementById("infobox")
+  };
+
 function ani(){
-  console.log("ok");
-  document.getElementById("p1i").classList.toggle("ani1");
-  document.getElementById("p2i").classList.toggle("ani2");
+  d.p1i.classList.toggle("ani1");
+  d.p2i.classList.toggle("ani2");
+  if(timeouts.p1){
+    clearTimeout(timeouts.p1);
+    toggleInfobox();
+    timeouts.p1 = null;
+  }
+  else timeouts.p1 = setTimeout(toggleInfobox, 700);
+}
+
+function toggleLocation(){
+  d.location.classList.toggle("hide");
+}
+
+function toggleInfobox(){
+  d.infobox.classList.toggle("show");
 }

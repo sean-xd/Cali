5970a68ff436d9f1c9e1766a120bcbcf582b08c4
Act <thecanbeat@gmail.com>
2016-08-23 14:09:21 -0600

    yay fixed it

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // Globals
 var timeouts = {},
   dom = {
+    p1: document.getElementById("p1"),
     p1i: document.getElementById("p1i"),
     p2i: document.getElementById("p2i"),
     infobox: document.getElementById("infobox")
@@ -15,6 +16,8 @@ function ani(){
 }
 
 function AreaToLocal(){
+  dom.p1.classList.toggle("hide");
+  dom.p1i.classList.remove("transit");
   dom.p1i.classList.toggle("ani1");
   dom.p2i.classList.toggle("ani2");
   if(!dom.p1i.classList.contains("ani1")){
@@ -31,6 +34,10 @@ function AreaToLocal(){
 function clr(e, arr){
   arr.forEach(_class => e.classList.remove(_class));
 }
+
+dom.p1i.addEventListener("mouseenter", e => {
+  dom.p1i.classList.add("transit");
+});
 
 dom.p1i.addEventListener("mousemove", e => {
   var x = Math.floor(e.pageX * -1 / 6),
